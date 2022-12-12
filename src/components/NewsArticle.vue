5a1821254c9fea2f2b880b6bae23fbfbab44f6d7
Alex Nico <a.nicolaidis@outlook.com>
2022-12-12 17:47:33 +1100

    created env variables

---
+++ src/components/NewsArticle.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>This is a news article</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="container">
-    <div class="left">This will have an image of stock/crypto</div>
-    <div class="right">This will have some sort of welcome messaege</div>
+    <div class="left"><img src="../assets/stock.png" /></div>
+    <div class="right">
+      <news-article></news-article>
+      <news-article></news-article>
+      <news-article></news-article>
+    </div>
   </div>
   <footer>Small bottom banner</footer>
 </template>
 
 <script>
+// import axios from "axios";
+import NewsArticle from "@/components/NewsArticle.vue";
+
 export default {
+  components: { NewsArticle },
   name: "home-page",
+  data() {
+    return {
+      article1: {},
+      article2: {},
+      article3: {},
+    };
+  },
+  async mounted() {
+    //this is requesting news articles form the api successfully
+    // const APIKEY = `${process.env.VUE_APP_MARKETAUX}`;
+    // const stockCode = "AMZN";
+    // const URL = `https://api.marketaux.com/v1/news/all?symbols=${stockCode}&filter_entities=true&language=en&api_token=${APIKEY}`;
+    // const response = await axios.get(URL);
+    // console.log(response);
+  },
 };
 </script>
 
@@ -16,5 +39,18 @@ export default {
 .container {
   display: flex;
   justify-content: space-evenly;
+}
+
+.left {
+  width: 500px;
+  padding: 25px;
+}
+.right {
+  width: 500px;
+  padding: 25px;
+}
+
+img {
+  width: 100%;
 }
 </style>
